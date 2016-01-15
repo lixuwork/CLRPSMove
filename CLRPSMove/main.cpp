7ffaeed32259413e12f18c92f6e209cd510560f5
BoJiang <188166614@qq.com>
2016-01-15 11:10:09 +0800

    Initial Commit

---
+++ CLRPSMove/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CLRPSMove
+//
+//  Created by vk on 16/1/15.
+//  Copyright © 2016年 clover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
